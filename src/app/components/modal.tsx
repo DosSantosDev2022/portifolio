bc92fdc548df3dc46492a2afc8d602d59ce02128
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-02-27 00:57:22 -0300

    styles : alteração de estilos no modal

--- src/app/components/modal.tsx
+++ src/app/components/modal.tsx
@@ -15,7 +15,7 @@ export function Modal() {
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="bg-blackA-blackA10 data-[state=open]:animate-ease-in fixed inset-0" />
-        <Dialog.Content className="data-[state=open]:animate-jump data-[state=open]:animate-duration-500 fixed top-[20%] left-[40%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+        <Dialog.Content className="data-[state=open]:animate-jump data-[state=open]:animate-duration-500 fixed top-[7%] left-[35%]  w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
           <div className="w-full text-end">
             <Dialog.Close asChild>
               <Button className="bg-zinc-900 text-zinc-50 hover:bg-zinc-700 rounded-full">
