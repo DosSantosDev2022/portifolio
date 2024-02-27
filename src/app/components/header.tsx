e12a9649ee38c6017bb0dc44fd42b417808edc4c
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-02-26 22:46:24 -0300

    styles : refatorando estilos do componente header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 export function Header() {
   const links = [
@@ -15,11 +18,16 @@ export function Header() {
       Url: '/Contact',
     },
   ]
+  const router = usePathname()
   return (
-    <header className="w-full h-[70px] bg-zinc-800 flex items-center p-2 ">
-      <nav className="flex gap-3">
+    <header className="w-full h-[70px] bg-zinc-800 flex items-center p-2 rounded-sm ">
+      <nav className="flex items-center justify-end w-full p-6 gap-3">
         {links.map((link) => (
-          <Link className="text-zinc-50" key={link.nome} href={link.Url}>
+          <Link
+            className={`text-zinc-50 hover:bg-purple-600 duration-500 transition-all p-1 rounded-sm ${router === link.Url ? 'bg-purple-600' : 'hover:bg-purple-600'}`}
+            key={link.nome}
+            href={link.Url}
+          >
             {link.nome}
           </Link>
         ))}
