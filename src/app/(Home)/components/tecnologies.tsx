7fe84582843b8404213694cdb86b2b63fdf25b18
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-03-04 22:12:23 -0300

    styles : corrigindo ordem das classes com plugin prettier tailwind

--- src/app/(Home)/components/tecnologies.tsx
+++ src/app/(Home)/components/tecnologies.tsx
@@ -9,12 +9,18 @@ interface TecnologiesProps {
 export function Tecnologies({ data }: TecnologiesProps) {
   const tech = data.technologies
   return (
-    <div className="mt-5 flex w-full flex-wrap gap-3">
-      {tech.map((tech) => (
-        <i key={tech.name} className="text-4xl text-zinc-50">
-          <MyTooltip icon={<CMSIcon icon={tech.iconSvg} />} nome={tech.name} />
-        </i>
-      ))}
+    <div className="w-full rounded-md bg-zinc-800 px-5 py-4 lg:h-36 ">
+      <h2 className="text-4xl font-bold text-zinc-50">Tecnologias</h2>
+      <div className="mt-5 flex w-full flex-wrap gap-3">
+        {tech.map((tech) => (
+          <i key={tech.name} className="text-4xl text-zinc-50">
+            <MyTooltip
+              icon={<CMSIcon icon={tech.iconSvg} />}
+              nome={tech.name}
+            />
+          </i>
+        ))}
+      </div>
     </div>
   )
 }
