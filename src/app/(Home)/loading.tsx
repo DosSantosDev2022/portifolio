34c9367c8c67c4a8e5ce214c35cb885baef559c4
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-03-06 01:14:08 -0300

    feat : criando componente inicial de loading

---
+++ src/app/(Home)/loading.tsx
@@ -0,0 +1,9 @@
+export default function HomePageLoading() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <p className="animate-bounce font-semibold text-zinc-50">
+        Carregando dados da homepage...
+      </p>
+    </div>
+  )
+}
---
+++ src/app/Projects/loading.tsx
@@ -0,0 +1,7 @@
+export default function ProjectsLoading() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <p className="font-bold text-zinc-50">Carregando projetos...</p>
+    </div>
+  )
+}
